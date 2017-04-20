fd0fa91b82899a838de63735059b6e3128789324
gustav <gustav.ortenberg@gmail.com>
2017-04-20 21:27:24 +0200

    semiworking bouncing balls

--- bouncing_balls/Animator.java
+++ bouncing_balls/Animator.java
@@ -11,76 +11,85 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import java.awt.image.BufferedImage;
+import java.awt.Rectangle;
+
 import bouncing_balls.Model.Ball;
 
-/**
- * Animated JPanel drawing the bouncing balls. No modifications are needed in this class.
- *  
- * @author Simon Robillard
- * 
- */
 @SuppressWarnings("serial")
 public final class Animator extends JPanel implements ActionListener {
 
+	private static final double pixelsPerMeter = 200;
+	private Model model;
+	private Timer timer;
+	private double deltaT;
+    private BufferedImage bImage;
+    private double lastTick;
+
 	public Animator(int pixelWidth, int pixelHeight, int fps) {
 		super(true);
-		this.timer = new Timer(1000 / fps, this);
-		this.deltaT = 1.0 / fps;
+
+        this.bImage = new BufferedImage(pixelWidth, pixelHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = (Graphics2D) bImage.getGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, bImage.getWidth(), bImage.getHeight());
+
+		this.timer = new Timer(200 / fps, this);
+		//this.timer = new Timer(33, this);
+		this.deltaT = 0.2 / fps;
 		this.model = new Model(pixelWidth / pixelsPerMeter, pixelHeight / pixelsPerMeter);
 		this.setOpaque(false);
 		this.setPreferredSize(new Dimension(pixelWidth, pixelHeight));
 	}
-	
-	/**
-	 * Drawing scale
-	 */
-	private static final double pixelsPerMeter = 200;
 
-	/**
-	 * Physical model
-	 */
-	private Model model;
-	
-	/**
-	 * Timer that triggers redrawing
-	 */
-	private Timer timer;
-	
-	/**
-	 * Time interval between redrawing, also used as time step for the model
-	 */
-	private double deltaT;
-	
+
 	public void start() {
+        lastTick = System.nanoTime();
 		timer.start();
 	}
 
-	public void stop() {
+    public void stop() {
     	timer.stop();
+    }
+
+    public void drawBalls(Color c) {
+        Graphics2D g = (Graphics2D) bImage.getGraphics();
+        g.setColor(c);
+		for (Ball b : model.balls) {
+			g.setColor(c);
+			double x = b.x - b.radius;
+			double y = b.y + b.radius;
+            double ix = x * pixelsPerMeter;
+            double iy = bImage.getHeight() - (y * pixelsPerMeter);
+            double size = b.radius * 2 * pixelsPerMeter;
+			Ellipse2D.Double e = new Ellipse2D.Double(ix, iy, size, size);
+			g.fill(e);
+            this.repaint(new Rectangle((int) ix-1, (int) iy-1, (int) size+2, (int) size+2));
+        }
+        g.dispose();
     }
 
 	@Override
 	protected void paintComponent(Graphics g) {
-		Graphics2D g2 = (Graphics2D) g;
-		// clear the canvas
-		g2.setColor(Color.WHITE);
-		g2.fillRect(0, 0, this.getWidth(), this.getHeight());
-		// draw balls
-		g2.setColor(Color.RED);
-		for (Ball b : model.balls) {
-			double x = b.x - b.radius;
-			double y = b.y + b.radius;
-			// paint balls (y-coordinates are inverted)
-			Ellipse2D.Double e = new Ellipse2D.Double(x * pixelsPerMeter, this.getHeight() - (y * pixelsPerMeter),
-					b.radius * 2 * pixelsPerMeter, b.radius * 2 * pixelsPerMeter);
-			g2.fill(e);
-		}
+        g.drawImage(bImage, 0, 0, null);
 	}
 	
     @Override
     public void actionPerformed(ActionEvent e) {
+        double tick = System.nanoTime();
+
+        drawBalls(Color.WHITE);
+
+        Graphics2D g = (Graphics2D) bImage.getGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, this.getWidth(), 50);
+        g.dispose();
+        this.repaint(new Rectangle(0, 0, this.getWidth(), 50));
+
     	model.step(deltaT);
-    	this.repaint();
+        drawBalls(Color.RED);
+
+        lastTick = tick;
     }
 	
 	public static void main(String[] args) {
@@ -88,7 +97,7 @@ public final class Animator extends JPanel implements ActionListener {
         // creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                Animator anim = new Animator(800, 600, 60);
+                Animator anim = new Animator(800, 600, 120);
                 JFrame frame = new JFrame("Bouncing balls");
             	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             	frame.add(anim);
--- bouncing_balls/Model.java
+++ bouncing_balls/Model.java
@@ -23,7 +23,7 @@ class Model {
 		// Initialize the model with a few balls
 		balls = new Ball[2];
 		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.2);
-		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3);
+		balls[1] = new Ball(2 * width / 3, height * 0.7, 1.3, 1.0, 0.3);
 	}
 
 	void step(double deltaT) {
@@ -36,13 +36,56 @@ class Model {
 			if (b.y < b.radius || b.y > areaHeight - b.radius) {
 				b.vy *= -1;
 			}
-			
 			// compute new position according to the speed of the ball
+			
+			for (Ball c : balls){
+				if ((b!=c)&&distance(b,c) <= (b.radius+c.radius)){
+					impact(b,c);
+				}
+			}
 			b.x += deltaT * b.vx;
-			b.y += deltaT * b.vy;
+			b.y += deltaT * b.vy;	
+			
 		}
 	}
-	
+
+	void impact(Ball a, Ball b){
+		double x = b.x-a.x;
+		double y = b.y-a.y;
+		double deg = Math.atan(y/x);
+				
+		double ahyp = a.vx*Math.cos(deg)-a.vy*Math.sin(deg);
+		double ananhyp = a.vx*Math.sin(deg)+a.vy*Math.cos(deg);
+		
+		double bhyp = b.vx*Math.cos(deg)-b.vy*Math.sin(deg);
+		double bnanhyp = b.vx*Math.sin(deg)+b.vy*Math.cos(deg);
+		
+		double tmp;
+		double am = a.radius*a.radius;
+		double bm = b.radius*b.radius;
+		
+		tmp = (bm*(bhyp-ahyp)+am*ahyp+bm*bhyp)/(am+bm);
+		bhyp = (am*(ahyp-bhyp)+am*ahyp+bm*bhyp)/(am+bm);
+		ahyp = tmp;
+
+		double avec = Math.sqrt(ahyp*ahyp+ananhyp*ananhyp);
+		double adeg = Math.atan(ananhyp/ahyp);
+		double bvec = Math.sqrt(bhyp*bhyp+bnanhyp*bnanhyp);
+		double bdeg = Math.atan(bnanhyp/bhyp);
+		a.vx= avec*Math.cos(adeg);
+		a.vy= avec*Math.sin(adeg);
+		b.vx= bvec*Math.cos(bdeg);
+		b.vy= bvec*Math.sin(bdeg);
+	}
+
+
+	double distance(Ball a, Ball b){
+		double x = a.x-b.x;
+		double y = a.y-b.y;
+		double d = Math.sqrt(x*x+y*y);
+		return d;
+	}
+
 	/**
 	 * Simple inner class describing balls.
 	 */
